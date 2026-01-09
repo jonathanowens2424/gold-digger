d6dfff1787c2f4e6bd7e71b82395a11b09cce2f3
Jonathan <jonathanlowens@live.com>
2026-01-09 10:26:03 -0600

    started figuring out directory paths

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 import http from "node:http";
 import generateGoldPrice from "./util/generateGoldPrice.js";
 
+const __dirname = import.meta.dirname;
+console.log(__dirname);
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
   const randomGoldPrice = generateGoldPrice();
-  res.end(`${randomGoldPrice}`);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(`<html><h1>Gold's price is ${randomGoldPrice}</h1></html>`);
 });
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
