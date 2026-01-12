850b0c3ba4fb04c1a36507ba21bb2386caa1cb03
Jonathan <jonathanlowens@live.com>
2026-01-12 12:11:17 -0600

    Added routing that logs different messages depending on the url.

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const PORT = 8000;
 
 const server = http.createServer((req, res) => {
   const randomGoldPrice = generateGoldPrice();
-  res.statusCode = 200;
+
   res.setHeader("Content-Type", "text/html");
+
+  if (req.url.startsWith("/gold-digger")) {
+    //serve HTML
+    res.statusCode = 200;
+  } else {
+    //serve 404.page
+    res.statusCode = 404;
+  }
   res.end(`<html><h1>Gold's price is ${randomGoldPrice}</h1></html>`);
 });
 
