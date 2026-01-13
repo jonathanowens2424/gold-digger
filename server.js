c755791112fd882e72b202b1ba851cde02443d61
Jonathan <jonathanlowens@live.com>
2026-01-13 08:24:42 -0600

    Added utility function that is dedicated to sending a response.

--- server.js
+++ server.js
@@ -4,29 +4,14 @@ import generateGoldPrice from "./util/generateGoldPrice.js";
 import serveStatic from "./util/serveStatic.js";
 import path from "node:path";
 import fs from "node:fs";
+import sendResponse from "./util/sendResponse.js";
 
 const __dirname = import.meta.dirname;
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  if (req.url.startsWith("/gold-digger")) {
-    const pathToResource = serveStatic(__dirname);
-    fs.readFile(pathToResource, "utf8", (err, content) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(content);
-    });
-  } else {
-    //find file path
-    //serve 404.page
-    res.setHeader("Content-Type", "text/html");
-    res.statusCode = 404;
-  }
+const server = http.createServer(async (req, res) => {
+  await serveStatic(req, res, __dirname);
 });
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
--- util/serveStatic.js
+++ util/serveStatic.js
@@ -1,5 +1,13 @@
 import path from "node:path";
+import sendResponse from "./sendResponse.js";
+import fs from "node:fs/promises";
 
-export default function serveStatic(baseDirectory) {
-  return path.join(baseDirectory, "public", "index.html");
+export default async function serveStatic(req, res, baseDirectory) {
+  const filePath = path.join(baseDirectory, "public", "index.html");
+  try {
+    const content = await fs.readFile(filePath);
+    sendResponse(res, 200, "text/html", content);
+  } catch (err) {
+    console.log(err);
+  }
 }
