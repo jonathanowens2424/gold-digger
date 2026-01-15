504b10774f2145a047d00e972aaae10c8e69c7c3
Jonathan <jonathanlowens@live.com>
2026-01-15 07:46:44 -0600

    added Close button for dialog popup

--- public/index.js
+++ public/index.js
@@ -1,7 +1,13 @@
 const investmentForm = document.querySelector("#investment-form");
+const closeButton = document.querySelector("#close-button");
 const dialog = document.querySelector("dialog");
 investmentForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  dialog.show();
+  dialog.showModal();
   console.log("You invested!");
+});
+
+closeButton.addEventListener("click", () => {
+  console.log("Test 1.");
+  dialog.close();
 });
